d633190893db7a00f7cacd2a10783885249a6a64
Elias Elfarri <47497819+Moelfarri@users.noreply.github.com>
2020-08-13 14:51:24 +0200

    Add files via upload

--- Effects_script/Splat.gd
+++ Effects_script/Splat.gd
@@ -4,6 +4,10 @@ extends Sprite
 
 func _ready():
 	$AnimationPlayer.play("FadeAway")
+	if name == "LifeAdded":
+		$LifeAddedSfx.play()
+	elif name == "LifeLost":
+		$LifeLostSfx.play()
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "FadeAway":
--- StageEndlessMode.gd
+++ StageEndlessMode.gd
@@ -3,26 +3,21 @@ extends Node2D
 # Sometimes the 4 in a row combo thing just stops playing if the threshold is gone beyond 100
 # Orange2D and swordslash sometimes the physics process overrides the area2d such that a cut does not occur and instead the orange is thrown away
 
-#BUGS I WANT TO FIX:
-#Remove_life gir noen ganger error siden texturen lages av appelsninen, finn en lurere måte å gjøre det på
+
 
 
 #TODO
 #RIGHT NOW:
-#MORE DIFFERENT IMPULSES ON ORANGE SPAWNS
-
-
+#Remove_life gir noen ganger error siden texturen lages av appelsninen, finn en lurere måte å gjøre det på
 
 #DIFFERENT MODES:
 	#1.TIME MODE:
 # 30 second timer!
 # ORANGES SPAWN IN WAVES
-#ORANGE SPAWNING DELAYMENT SO THEY DONT SPAWN AS YOU CUT ONE ORANGE
 # ADD GRADING (NO HIGHSCORE)
+#if lose no lfie = A+ , LOSE 1 life B, lose 2 lives C lose 3 lives or bomb gets F
 
-	#2.ENDLESS MODE:
-#THINGS JUST SPAWN ENDLESSLY, LIKE A TRAINING MODE.
-#HIGSCORE
+
 
 #SHOP: 
 #BUY UPGRADE THAT DONT DO SHIT -> CURE SCURVEY 
@@ -31,13 +26,10 @@ extends Node2D
 
 
 
+
 #REVAMP CODE:
-#REMOVE THE "TUTORIAL SCREEN AND CHANGE INTO - TIME MODE VS ENDLESS MODE"
-#CLEAN UP STAGE SOUND NODES
+# CHANGE INTO - TIME MODE VS ENDLESS MODE" LET MAIN SCREEN GO TO IT AND U SLASH WHICH ONE U WANT
 #CLEAN UP IN DOCUMENT NAMES AND FILE NAMES
-#MAKE SWORD INTO AN INSTANCE BASED ON CLICKS THAN IT EXISTING NONSTOP THIS WAY YYOU CAN HAVE MULTIPLE INSTANCES ON ANDROID?
-
-
 
 
 #Testing:
@@ -63,11 +55,13 @@ const FOURx = preload("res://Effects_script/4x.tscn")
 const LIFE_LOST = preload("res://Effects_script/LifeLost.tscn")
 const LIFE_ADDED = preload("res://Effects_script/LifeAdded.tscn")
 
+#UI
+const PAUSE_DASHBOARD = preload("res://UI_script/PauseDashboard.tscn")
 
 var orange_sliced_position = Vector2()
 var	orange_sliced_angle = 0.0
 var oranges_spawned = 0
-var orange_threshold = 2
+var orange_threshold = 1
 
 var is_bomb_sliced = false
 var bomb_sliced_position = Vector2()
@@ -75,6 +69,8 @@ var bomb_sliced_position = Vector2()
 var random_generator = RandomNumberGenerator.new()
 var lives = 3 
 
+var spawn_timer = 0
+var timer_threshold = 600
 
 enum SPAWNER{FAR_LEFT, LEFT,  MIDDLE, FAR_RIGHT, RIGHT}
 
@@ -84,14 +80,12 @@ func _ready():
 	OS.low_processor_usage_mode = true
 	random_generator.randomize()
 	
-	
 	if AudioServer.is_bus_mute(AudioServer.get_bus_index("Master")):
 		$MuteButton.visible = false
 		$UnmuteButton.visible = true
 
 
 func _process(delta):
-	
 	#Score display
 	$ScoreCounterText.text = str(Global.score) + "x"
 	
@@ -102,9 +96,16 @@ func _process(delta):
 	
 	
 	#Orange spawning sequence
-	if oranges_spawned != orange_threshold:
-		randomly_spawn_and_project_orange()
-		oranges_spawned += 1
+	orange_spawn_timer()
+	if oranges_spawned != orange_threshold and not orange_threshold == 0:
+		var spawn_probability = random_generator.randf_range(1,100)
+		#2.5% change of spawning a bomb instead 
+		if spawn_probability <= 97.5:
+			randomly_spawn_and_project_orange()
+			oranges_spawned += 1
+		else:
+			randomly_spawn_and_project_bomb()
+
 	
 	
 	#COMBO GRAPHICS AND SOUNDS
@@ -203,20 +204,19 @@ func add_life():
 	lives += 1
 	var added_life = LIFE_ADDED.instance()
 	add_child(added_life)
-	$AddLifeSoundEffect.play()
 
 
 func remove_life():
 	lives -= 1
 	var lost_life = LIFE_LOST.instance()
-	get_parent().add_child(lost_life) #since it is used in orange2d we need to use get_parent()
-	$LostLifeSoundEffect.play()
-	
+	add_child(lost_life) #since it is used in orange2d we need to use get_parent()
+
 
 ###############ORANGE MANGEMENT##################
 func randomly_spawn_and_project_orange():
 	var orange = ORANGE.instance()
 	var random_position = random_generator.randi_range(0,4)
+	var binary_horisontal_impulse = random_generator.randi_range(0,1)
 	
 	match random_position:
 		SPAWNER.FAR_LEFT:
@@ -225,7 +225,10 @@ func randomly_spawn_and_project_orange():
 			$OrangeThrowSoundEffect.pitch_scale = 0.8
 		SPAWNER.LEFT:
 			orange.position = $Spawners/LeftSpawner.position
-			orange.apply_central_impulse(Vector2(100,-400))
+			if binary_horisontal_impulse == 0:
+				orange.apply_central_impulse(Vector2(100,-400))
+			else:
+				orange.apply_central_impulse(Vector2(0,-400))
 			$OrangeThrowSoundEffect.pitch_scale = 0.9
 		SPAWNER.MIDDLE:
 			orange.position = $Spawners/MiddleSpawner.position
@@ -233,7 +236,10 @@ func randomly_spawn_and_project_orange():
 			$OrangeThrowSoundEffect.pitch_scale = 1.0
 		SPAWNER.RIGHT:
 			orange.position = $Spawners/RightSpawner.position
-			orange.apply_central_impulse(Vector2(-100,-400))
+			if binary_horisontal_impulse == 0:
+				orange.apply_central_impulse(Vector2(-100,-400))
+			else:
+				orange.apply_central_impulse(Vector2(0,-400))
 			$OrangeThrowSoundEffect.pitch_scale = 1.1
 		SPAWNER.FAR_RIGHT:
 			orange.position = $Spawners/FarRightSpawner.position
@@ -245,22 +251,50 @@ func randomly_spawn_and_project_orange():
 	$OrangeThrowSoundEffect.play()
 
 
+func orange_spawn_timer():
+	spawn_timer += 1
+	if spawn_timer >= timer_threshold:
+		spawn_timer = 0
+		if timer_threshold == 600:
+			orange_threshold = 0
+			timer_threshold = 150
+		else:
+			var threshold_probability = random_generator.randf_range(1,100)
+			if threshold_probability <= 50:
+				set_orange_spawn_threshold(1)
+			elif threshold_probability > 50 and threshold_probability <= 90:
+				set_orange_spawn_threshold(2)
+			elif threshold_probability > 90 and threshold_probability <= 97.5:
+				set_orange_spawn_threshold(3)
+			else:
+				set_orange_spawn_threshold(4)
+			timer_threshold = 600
+
+
 func _on_orange_sliced(given_position, given_sliced_angle):
 	orange_sliced_position = given_position
 	orange_sliced_angle = given_sliced_angle
-	
-	
+
+
+func set_orange_spawn_threshold(threshold):
+	orange_threshold = threshold
+
 ###############BUTTONS##################
 func _on_PauseButton_pressed():
 	$PauseButton.visible = false
 	$ResumeButton.visible = true
+	var pause_dashboard = PAUSE_DASHBOARD.instance()
+	add_child(pause_dashboard)
 	get_tree().paused = true
+
 
 
 func _on_ResumeButton_pressed():
 	$PauseButton.visible = true
 	$ResumeButton.visible = false
+	get_node("PauseDashboard").queue_free()
 	get_tree().paused = false
+
 
 
 func _on_MuteButton_pressed():
@@ -276,9 +310,9 @@ func _on_UnmuteButton_pressed():
 
 
 ###############BOMB##################
-func _on_BombSpawnTimer_timeout():
+func  randomly_spawn_and_project_bomb():
 	var bomb = BOMB.instance()
-	var random_position = random_generator.randi_range(0,4)
+	var random_position = random_generator.randi_range(1,4)
 	
 	match random_position:
 		SPAWNER.FAR_LEFT:
@@ -288,8 +322,8 @@ func _on_BombSpawnTimer_timeout():
 			bomb.position = $Spawners/LeftSpawner.position
 			bomb.apply_central_impulse(Vector2(100,-400))
 		SPAWNER.MIDDLE:
-			bomb.position = $Spawners/MiddleSpawner.position
-			bomb.apply_central_impulse(Vector2(0,-400))
+			bomb.position = $Spawners/LeftSpawner.position
+			bomb.apply_central_impulse(Vector2(0,-200))
 		SPAWNER.RIGHT:
 			bomb.position = $Spawners/RightSpawner.position
 			bomb.apply_central_impulse(Vector2(-100,-400))
@@ -316,3 +350,7 @@ func game_over():
 			set_process(false)
 		else:
 			return
+
+
+
+
